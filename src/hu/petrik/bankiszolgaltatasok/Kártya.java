4e15d8fda6cc5d0deb39f24ded1776306ea06dc8
kilianmarcell <kilian.marcell.andras@gmail.com>
2021-10-24 10:48:42 +0200

    Kártya osztály kész

--- src/hu/petrik/bankiszolgaltatasok/Kártya.java
+++ src/hu/petrik/bankiszolgaltatasok/Kártya.java
@@ -1,4 +1,24 @@
 package hu.petrik.bankiszolgaltatasok;
 
-public class Kártya {
+public class Kártya extends BankiSzolgáltatás {
+    private String kartyaSzam;
+    private Számla s;
+
+    public Kártya(Tulajdonos t, String kartyaSzam, Számla s) {
+        super(t);
+        this.kartyaSzam = kartyaSzam;
+        this.s = s;
+    }
+
+    public String getKartyaSzam() {
+        return kartyaSzam;
+    }
+
+    public boolean Vásárlás(double osszeg) {
+        if (s.Kivesz(osszeg)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
