7575de435a161f4a9c169850c7d77d4a9f4ae41e
kilianmarcell <kilian.marcell.andras@gmail.com>
2021-10-24 10:28:49 +0200

    MegtakarításiSzámla osztály létrehozva

---
+++ src/hu/petrik/bankiszolgaltatasok/MegtakarításiSzámla.java
@@ -0,0 +1,25 @@
+package hu.petrik.bankiszolgaltatasok;
+
+public class MegtakarításiSzámla extends Számla {
+    private static double kamat;
+
+    public MegtakarításiSzámla(Tulajdonos t) {
+        super(t);
+        this.kamat = 1;
+    }
+
+    public double getKamat() {
+        return kamat;
+    }
+
+    public void setKamat(double kamat) {
+        this.kamat = kamat;
+    }
+
+    public double Kamatjóváírás() {
+        if (Kivesz(getKamat())) {
+            setKamat(getEgyenleg() * kamat);
+            return getKamat();
+        }
+    }
+}
